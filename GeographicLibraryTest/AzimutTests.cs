263ce5ba9da59438c39a1650e1384d020e2c26c2
Студент <class@vyatsu.ru>
2024-02-19 12:07:26 +0300

    r4

---
+++ GeographicLibraryTest/AzimutTests.cs
@@ -0,0 +1,25 @@
+﻿using GeographicLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace GeographicLibraryTest
+{
+    [TestClass]
+    class AzimutTests
+    {
+        [TestMethod]
+        [DynamicData(nameof(GetPoints), DynamicDataSourceType.Method)]
+        public void Test_CreateArc_CreatedArcNotNull(Point a, Point b)
+        {
+            Arc arc = new Arc(a, b);
+
+            Assert.IsNotNull(arc)
+        }
+
+        public static IEnumerable<List<Point>> GetPoints()
+        {
+            yield return new List<Point>() { new Point(0.0, 180.0), new Point(0.0, 90.0) };
+            yield return new List<Point>() { new Point(0.0, -90.0), new Point(0.0, 90.0) };
+        }
+    }
+}
