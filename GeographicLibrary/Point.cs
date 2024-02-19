8a40a959be688110dbafe82f67f353c740837b36
Студент <class@vyatsu.ru>
2024-02-19 11:20:16 +0300

    Всё до ref2 Включительно

---
+++ GeographicLibrary/Point.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace GeographicLibrary
+{
+    public class Point
+    {
+        double latitude;
+        double longitude;
+
+        public double Longitude 
+        { 
+            get => longitude; 
+            set
+            {
+                if (Math.Abs(value) > 180)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                longitude = value;
+            }
+        }
+        public double Latitude 
+        {   
+            get => latitude; 
+            set
+            {
+                if (Math.Abs(value) > 90)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                latitude = value;
+            } 
+        }
+
+        public Point(double latitude, double longitude)
+        {
+            if (Math.Abs(latitude) > 90 || Math.Abs(longitude) > 180)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+
+        }
+    }
+}
---
+++ GeographicLibraryTest/UnitTestPoint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using GeographicLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GeographicLibraryTest
+{
+    [TestClass]
+    public class UnitTestPoint
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMethodCtor_ThrowsArgumentOutOfRangeException()
+        {
+            Point p;
+
+            double a = -1000.0;
+            double b = -1000.0;
+
+            p = new Point(a, b);
+        }
+
+        [TestMethod]
+        public void TestMethodCtor_CteatePoin()
+        {
+            Point p;
+
+            double a = 0.0;
+            double b = 0.0;
+
+            p = new Point(a, b);
+
+            Assert.IsNotNull(p);
+        }
+    }
+}
